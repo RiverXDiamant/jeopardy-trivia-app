f65bdd987637e601df28a763021702bc0a0a9a09
Alex <eddings.sharanna@gmail.com>
2022-12-11 16:49:16 -0500

    updated Score component

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,12 +1,32 @@
+import React from "react";
+// import useState
+import { useState } from "react";
+
 export default function Score() {
+  const [score, setScore] = useState(0);
+  // score - state
+  // setScore = updated score amount
+
+  // event handles
+  const handleDecrement = () => {
+    setScore(score - 100);
+  };
+
+  const handleIncrement = () => {
+    setScore(score + 100);
+  };
   return (
     <div className="score-counter">
       <h2>
-        <strong>Score:</strong>
+        <strong>Score: {score}</strong>
       </h2>
       <div className="score-buttons">
-        <button id="score-decrease">Decrease</button>
-        <button id="score-increase">Increase</button>
+        <button id="score-decrease" onClick={handleDecrement}>
+          Decrease
+        </button>
+        <button id="score-increase" onClick={handleIncrement}>
+          Increase
+        </button>
         <button id="score-reset">Reset</button>
       </div>
     </div>
