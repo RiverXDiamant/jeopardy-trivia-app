67528ae11f1748022a4bccdf36416b3ec37cab66
Alex <eddings.sharanna@gmail.com>
2022-12-13 18:55:54 -0500

    updated Question component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,21 +5,27 @@ import { useState } from "react";
 import Categories from "./Category";
 import Points from "./Points";
 import Answers from "./Answer";
+// import component
 
 export default function GetQuestion() {
   const [question, setQuestion] = useState(null);
 
-  const getQuestion = async () => {
+  const fetchQuestion = async () => {
     const response = await fetch(" https://jservice.io/api/random");
     const data = await response.json();
     setQuestion(data);
+  };
+  const [showQuestion, setShowQuestion] = useState(null);
+
+  const revealQuestion = () => {
+    setShowQuestion((toggle) => !toggle);
   };
   return (
     <div className="get-question">
       <h2>Let's Play! </h2>
 
-      <h2>Question: {question}</h2>
-      <button onClick={getQuestion}>Get Question</button>
+      <h2>{showQuestion && <h6>Question:{question}</h6>}</h2>
+      <button onClick={revealQuestion}>Get Question</button>
       <Categories />
       <Points />
       <Answers />
